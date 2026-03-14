cc7785169048c0850f72ab3df68c5f9dd2e29a2e
Amit Kumar <amitkumar753197@gmail.com>
2026-03-14 11:33:32 +0530

    Initialise the springboot

---
+++ src/test/java/com/coding/youtube/practice/LearningRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.youtube.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
