3df9ff0f7927feb1741968c4d8ee2a27c6acd5e4
amitkumar0009 <amitkumar753197@gmail.com>
2026-05-05 10:57:57 +0530

    added modelmapper

--- src/main/java/com/cwm/studentmanagement/config/ModelMapperConfig.java
+++ src/main/java/com/cwm/studentmanagement/config/ModelMapperConfig.java
@@ -4,10 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/*
- * Copyright (c) 2026 Mahesh Shet
- * Licensed under the MIT License.
- */
+
 
 @Configuration
 public class ModelMapperConfig {
